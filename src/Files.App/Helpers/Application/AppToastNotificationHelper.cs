31b80f2100c70321823ccd92e9501049385fae8e
liuzilong <liuzilong@local>
2026-09-15 21:41:28 +0800

    feat: unify rounded app icon and notification branding

--- src/Files.App/Helpers/Application/AppToastNotificationHelper.cs
+++ src/Files.App/Helpers/Application/AppToastNotificationHelper.cs
@@ -5,12 +5,14 @@ namespace Files.App.Helpers.Application
 {
 	internal static class AppToastNotificationHelper
 	{
+		private static readonly Uri ApplicationLogoUri = new("ms-appx:///Assets/AppTiles/Dev/Square44x44Logo.scale-100.png");
+
 		public static void ShowUnhandledExceptionToast()
 		{
 			var toastContent = new AppNotificationBuilder()
 					.AddText(Strings.ExceptionNotificationHeader.GetLocalizedResource())
 					.AddText(Strings.ExceptionNotificationBody.GetLocalizedResource())
-					.SetAppLogoOverride(new Uri("ms-appx:///Assets/error.png"))
+					.SetAppLogoOverride(ApplicationLogoUri)
 					.AddButton(new AppNotificationButton(Strings.ExceptionNotificationReportButton.GetLocalizedResource())
 						.SetInvokeUri(new Uri(Constants.ExternalUrl.BugReportUrl)))
 					.BuildNotification();
@@ -22,6 +24,7 @@ namespace Files.App.Helpers.Application
 			var toastContent = new AppNotificationBuilder()
 				.AddText(Strings.BackgroundRunningNotificationHeader.GetLocalizedResource())
 				.AddText(Strings.BackgroundRunningNotificationBody.GetLocalizedResource())
+				.SetAppLogoOverride(ApplicationLogoUri)
 				.BuildNotification();
 			AppNotificationManager.Default.Show(toastContent);
 		}
@@ -31,6 +34,7 @@ namespace Files.App.Helpers.Application
 			var toastContent = new AppNotificationBuilder()
 				.AddText(Strings.EjectNotificationHeader.GetLocalizedResource())
 				.AddText(Strings.EjectNotificationBody.GetLocalizedResource())
+				.SetAppLogoOverride(ApplicationLogoUri)
 				.SetAttributionText("SettingsAboutAppName".GetLocalizedResource())
 				.BuildNotification();
 			AppNotificationManager.Default.Show(toastContent);
